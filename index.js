f07893241d22e836624408760e38a4f441a76646
leesochay <sochaylee@gmail.com>
2023-06-11 16:53:47 -0400

    updating comments for the logo text validation

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ inquirer
         message: "Please enter the text for the logo (up to three characters).",
         name: "logoText",
         // validate function for text input of up to three characters
+        // From npm: validate: (Function) Receive the user input and answers hash.
+        // Should return true if the value is valid, and an error message (String) otherwise.
+        // If false is returned, a default error message is provided.
         validate: function(logoText){
           if (logoText.length > 3){
               return "Logo text must be only up to three characters.";
